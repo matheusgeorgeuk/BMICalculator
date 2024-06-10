8ae6b80216dcc7341dbb93dc91b1efa324b6a17f
matheusgeorgeuk <matheusgeorgeuk@gmail.com>
2024-06-10 10:51:32 +0100

    EditText home screen + BMI calculation

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,36 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Retrieve the components EditText
+        // Create a variable and associate (=) the UI<EditText> component
+        // Recover the screen button
+
+        // Put action on the button setOnClickListener
+        // Recover the text typed in edtWeight
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edt_height)
+
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+
+        btnCalculate.setOnClickListener {
+        val weight: Float = edtWeight.text.toString().toFloat()
+        val height: Float = edtHeight.text.toString().toFloat()
+
+        val heightQ2 = height * height
+        val result = weight / heightQ2
+
+        println("Acao do botao" + result)
+
+        }
     }
 }
