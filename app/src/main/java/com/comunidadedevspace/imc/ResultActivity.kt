a5cbcf21e118693a2a71d7f43ca9fd12e4ab41d5
matheusgeorgeuk <matheusgeorgeuk@gmail.com>
2024-06-29 09:44:06 +0100

    Improving the UX

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -21,17 +22,29 @@ class ResultActivity : AppCompatActivity() {
         val tvClassification = findViewById<TextView>(R.id.tv_classification)
         tvResult.text = result.toString()
 
-        val classification: String = if (result <= 18.5f) {
-            "THINNESS"
-        } else if (result > 18.5f && result <= 24.9f) {
-            "NORMAL"
-        } else if (result > 25f && result <= 29.9f) {
-            "OVERWEIGHT"
-        } else if (result > 30f && result <= 39.9f) {
-        "OBESITY"
-        } else {
-            "SEVERE OBESITY"
+        var (classification: String , color: Int) = when {
+            result <= 18.5f -> {
+                "UNDERWEIGHT" to R.color.underweight
+            }
+
+            result > 18.5f && result <= 24.9f -> {
+                "NORMAL" to R.color.normal
+            }
+
+            result > 25f && result <= 29.9f -> {
+                "OVERWEIGHT" to R.color.overweight
+            }
+
+            result > 30f && result <= 39.9f -> {
+                "OBESITY" to R.color.obesity
+            }
+
+            else -> {
+                "SEVERE OBESITY" to R.color.severe_obesity
+            }
         }
+
+        tvClassification.setTextColor(ContextCompat.getColor(this, color))
 
         tvClassification.text = classification
 
